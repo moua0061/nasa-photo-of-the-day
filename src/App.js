f927b19adac0751f3044baca9b20f61dac014485
Lia Moua <moua0061@yahoo.com>
2021-10-06 16:56:49 -0400

    got picture to show

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ function App() {
   const [nasaData, setNasaData] = useState([]);
   const [nasaInfo, setNasaInfo] = useState([]);
 
-  axios.get(`${baseUrl}${keyConnector}${apiKey}`)
+
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=MfEb6c9Y3m7bFaSeflHCOH21fXiWNhXgD94LRO4K')
   .then(resp => {
-    console.log(resp);
+    console.log(resp.data);
+    return setNasaData(resp.data);
   })
   .catch (err => {
     console.log('your shit ain\'t working!!');
+  
   })
 
 
@@ -26,6 +29,12 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <div className='container'>
+        <div className='APOD'>
+          <h1> Picture of the day!</h1>
+          <img className="APOD_image" alt='APOD image' src={`${nasaData.url}`} />
+        </div>
+      </div>
     </div>
   );
 }
